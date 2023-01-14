26ebd861ad77a1288d44bb4ce2509dc1f3e9cdf2
azart6314 <azart6314@gmail.com>
2023-01-14 18:08:53 +0300

    initial

--- src/test/java/tests/ActionsTest.java
+++ src/test/java/tests/ActionsTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.List;
 
 public class ActionsTest extends BaseTest {
@@ -24,15 +25,31 @@ public class ActionsTest extends BaseTest {
                 .build()
                 .perform(); //выполнить . только в этот момент происходят действия
 
-        Assert.assertTrue(waitsService.waitForElementInvisible(targetElements.get(0)));
+        Assert.assertTrue(waitsService.waitForElementInvisible(targetElements.get(0))); //дождались что элемент пропал
     }
 
-    @Test
-    public void fileUploadTest() throws InterruptedException {
+    @Test // более не верный тест ( ниже правильнее)
+    public void fileUploadTest1() throws InterruptedException {
         driver.get("http://the-internet.herokuapp.com/upload");
 
         WebElement fileUploadElement = waitsService.waitForExists(By.xpath("//input[@type='file']"));
-        String pathToFile = ActionsTest.class.getClassLoader().getResource("download.jpeg").getPath();
+        String pathToFile = "src" + File.separator + "test/resources/download.jpeg";
+        System.out.println(pathToFile);
+
+        fileUploadElement.sendKeys(pathToFile);
+        waitsService.waitForVisibilityBy(By.id("file-submit")).submit();
+
+        Thread.sleep(5000);
+        Assert.assertEquals(waitsService.waitForVisibilityBy(By.id("uploaded-files")).getText().trim(),
+                "download.jpeg");
+    }
+
+    @Test
+    public void fileUploadTest() throws InterruptedException {  //загрузка файла. Ищес элемент с отрибутов файл
+        driver.get("http://the-internet.herokuapp.com/upload");
+
+        WebElement fileUploadElement = waitsService.waitForExists(By.xpath("//input[@type='file']"));
+        String pathToFile = ActionsTest.class.getClassLoader().getResource("download.jpeg").getPath(); //Путь к файлу. getPath -возвращает обсалютный путь к этому файлу в системе (в независимости где будет запускаться код )
         System.out.println(pathToFile);
 
         fileUploadElement.sendKeys(pathToFile);
@@ -41,4 +58,5 @@ public class ActionsTest extends BaseTest {
         Assert.assertEquals(waitsService.waitForVisibilityBy(By.id("uploaded-files")).getText().trim(),
                 "download.jpeg");
     }
+
 }
