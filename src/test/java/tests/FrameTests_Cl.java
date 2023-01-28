543c816cdfe572390631fa9a2543237be3518146
azart6314 <azart6314@gmail.com>
2023-01-28 17:10:06 +0300

    initial

--- src/test/java/tests/FrameTests_Cl.java
+++ src/test/java/tests/FrameTests_Cl.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class FrameTests extends BaseTest {
+public class FrameTests_Cl extends BaseTest {
 
     @Test
     public void frameTest() {
