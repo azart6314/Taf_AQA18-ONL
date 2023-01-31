c281fa095b58eeb3fbfd181eb6f8dbec3796881c
azart6314 <azart6314@gmail.com>
2023-01-31 19:22:23 +0300

    initial

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,9 +1,7 @@
 package baseEntities;
 
 import Factory.BrowserFactory;
-import Utils.InvokedListener;
 import configuration.ReadProperties;
-import io.qameta.allure.Attachment;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -11,51 +9,37 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import services.WaitsService;
+import steps.ProjectSteps;
 import steps.UserStep;
-
-@Listeners(InvokedListener.class)
-
+@Listeners()
 public class BaseTest {
 
     protected WebDriver driver;
     protected UserStep userStep;
     protected WaitsService waitsService;
+    protected ProjectSteps projectSteps;
 
 
 
     @BeforeMethod
     public void setUp(ITestContext iTestContext) {   /*ITestContext   iTestContext  сделано для снятия скринов при ошибка */
         driver = new BrowserFactory().getDriver();
-        driver.get(ReadProperties.getUrl());
         waitsService = new WaitsService(driver);
+        driver.get(ReadProperties.getUrl());
+
 
         //Solution1     //сделано тоже для скринов в InvokedListener
         iTestContext.setAttribute("driver", driver);
         //Solution1 - Finish
 
         userStep = new UserStep(driver);
-        System.out.println(driver.hashCode());
+        //System.out.println(driver.hashCode());
+        projectSteps = new ProjectSteps(driver);
 
     }
 
     @AfterMethod
-    public void tearDown(ITestResult testResult) { //ITestResult testResult также все для скиринов
-        // Solution -2: Плохое решение - потому что Screenshot добавляется в шаг TearDown
-        /* if (testResult.getStatus()== ITestResult.FAILURE){
-        try {
-        byt[] srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        saveScreenshot(srcFile);
-        } catch (NiSuchSessionException ex) {
-        }
-        }
-         */
-
+    public void tearDown(ITestResult testResult) { //ITestResult testResult - для скринов
         driver.quit();
-    }
-
-    // Solution -2
-    @Attachment(value = "Page screenshot", type = "image/png") // этот метод для картинки
-    private byte[] saveScreenshot(byte[] screenshot) {
-        return screenshot;
     }
 }
